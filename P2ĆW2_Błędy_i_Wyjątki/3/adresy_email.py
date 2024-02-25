b039b845cec59e9643ee6199e298b307eaf88b0c
Fiksuu <piotrfiks560@gmail.com>
2024-02-25 10:21:19 +0100

    modified:   "P2\304\206W2_B\305\202\304\231dy_i_Wyj\304\205tki/3/adresy_email.py"

--- P2ĆW2_Błędy_i_Wyjątki/3/adresy_email.py
+++ P2ĆW2_Błędy_i_Wyjątki/3/adresy_email.py
@@ -1,31 +1,15 @@
 import re
 
-def sprawdz_email(email):
-    wzorzec = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    if re.match(wzorzec, email):
-        return True
-    return False
-
-def wczytaj_plik(nazwa_pliku):
-    try:
-        with open(nazwa_pliku, 'r') as plik:
-            adresy_email = plik.readlines()
-    except FileNotFoundError:
-        print("Plik nie istnieje.")
-        return []
-    except Exception as e:
-        print(f"Wystąpił błąd podczas odczytu pliku: {str(e)}")
-        return []
-    
-    return [email.strip() for email in adresy_email]
-
-def sprawdz_plik(nazwa_pliku):
-    adresy_email = wczytaj_plik(nazwa_pliku)
-    for email in adresy_email:
-        if sprawdz_email(email):
-            print(f"Adres email {email} jest poprawny.")
+try:
+    with open('P2ĆW2_Błędy_i_Wyjątki/3/adresyemail.txt', 'r') as f:
+        lines = f.readlines()
+    pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+    for line in lines:
+        if pattern.fullmatch(line.strip()):
+            print(f'Adres e-mail {line.strip()} jest poprawny.')
         else:
-            print(f"Adres email {email} jest niepoprawny.")
-
-nazwa_pliku = 'adresyemail.txt'
-sprawdz_plik(nazwa_pliku)
+            print(f'Adres e-mail {line.strip()} jest niepoprawny.')
+except FileNotFoundError:
+    print('Plik nie istnieje.')
+except Exception as e:
+    print(f'Wystąpił błąd: {e}')
