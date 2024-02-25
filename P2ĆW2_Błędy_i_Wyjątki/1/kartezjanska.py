a66a15ac7dc5f248026dec82e35bdc5ff2c9f177
Fiksuu <piotrfiks560@gmail.com>
2024-02-25 10:04:38 +0100

    modified:   "P2\304\206W2_B\305\202\304\231dy_i_Wyj\304\205tki/1/kartezjanska.py"

--- P2ĆW2_Błędy_i_Wyjątki/1/kartezjanska.py
+++ P2ĆW2_Błędy_i_Wyjątki/1/kartezjanska.py
@@ -1,8 +1,8 @@
 import math
 
 try:
-    x1, y1 = map(float, input("Podaj współrzędne pierwszego punktu (x, y): ").split())
-    x2, y2 = map(float, input("Podaj współrzędne drugiego punktu (x, y): ").split())
+    x1, y1 = map(float, input("Podaj współrzędne pierwszego punktu (x y): ").split())
+    x2, y2 = map(float, input("Podaj współrzędne drugiego punktu (x y): ").split())
     distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     print(f"Odległość między punktami wynosi: {distance}")
 except ValueError:
